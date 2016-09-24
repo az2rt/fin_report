06cdf9e8e53509e3795761329aaf648e598a9774
i.s.kudrjashov@gmail.com <mk11kfguv>
2016-09-25 01:08:50 +0300

    запилил сортировку

--- report.py
+++ report.py
@@ -8,18 +8,14 @@ import xlrd
 
 rb = xlrd.open_workbook('c:/exportXLS.xls',formatting_info=True,encoding_override='cp1251')
 # не правильное считается дата
-sheet = rb.sheet_by_index(0)
-
+sheet = rb.sheet_by_index(1)
 lst=[sheet.row_values(rownum)for rownum in range(sheet.nrows)]
 dict={}
-
-
 for x in lst:
     if dict.has_key(x[3]):
         dict[x[3]] = dict[x[3]]+abs(x[4])
-
     else:
         dict[x[3]] = abs(x[4])
 
-for x in dict:
+for x in sorted(dict, key=dict.__getitem__, reverse=True):
     print("%s = %s " % (x, dict[x]))
