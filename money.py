0eb1465c47b5bda0d9eccd5cd4dd8994a00f4220
i.kudryashov <i.kudryashov@corp.mail.ru>
2017-05-01 00:27:13 +0300

    добавил отчет по месяцу, порефакторил чутка

--- money.py
+++ money.py
@@ -1,22 +1,24 @@
+# -*- coding: utf-8 -*-
 import json
 import sqlite3
 from datetime import datetime as dt
-year = ['2015','2016','2017']
+
+year = ['2015', '2016', '2017']
 con = sqlite3.connect('test7.db')
 cur = con.cursor()
 months={
-    'january':  '01,01,31',
-    'february': '02,01,28',
-    'march':    '03,01,30',
-    'april':    '04,01,31',
-    'may':      '05,01,30',
-    'june':     '06,01,31',
-    'july':     '07,01,30',
-    'august':   '08,01,31',
-    'september':'09,01,30',
-    'october':  '10,01,31',
-    'november': '11,01,30',
-    'december': '12,01,31'
+    'january':  ['01','01','31'],
+    'february': ['02','01','28'],
+    'march':    ['03','01','30'],
+    'april':    ['04','01','31'],
+    'may':      ['05','01','30'],
+    'june':     ['06','01','31'],
+    'july':     ['07','01','30'],
+    'august':   ['08','01','31'],
+    'september':['09','01','30'],
+    'october':  ['10','01','31'],
+    'november': ['11','01','30'],
+    'december': ['12','01','31']
 }
 
 
@@ -45,28 +47,50 @@ def mysql_fill():
         con.commit()
     cur.close()
 
-
+def mysql_select():
+    """
+    общая функция для запросов
+    :return:
+    """
 def get_category():
     return cur.execute('select * from category')
 
+def last_first_day(year, month):
+    first = year + '-' + month[0] + '-' + month[1]
+    last = year + '-' + month[0] + '-' + month[2]
+    return first, last
 
 def report_by_all_month():
     for current_year in year:
-        for month, number in months.items():
-            day_month = number.split(",")
-            first = current_year+'-'+day_month[0]+'-'+day_month[1]
-            last = current_year+'-'+day_month[0]+'-'+day_month[2]
+        for month in months.values():
+            first, last = last_first_day(current_year, month)
             result = cur.execute("SELECT sum(sum) from transactions t join category c on t.category_id = c.id  "
                                  "where t.date between ? and ? and t.type = 2 ", (first, last,)).fetchall()
             if result[0][0] is not None: print(current_year, month, round(result[0][0], 2))
-    con.close()
+
+
+def report_by_month(year,month):
+    first, last = last_first_day(year, month)
+    print(first,last)
+    result = cur.execute("SELECT sum(sum) from transactions where date between  ? and ?", (first, last,)).fetchall()
+    print(result)
+
+
+def report_by_income():
+    pass
+
+
+def get_month_day(month):
+    return months[month]
 
 """
 написать еще очет за месяц по категориям
 отчет по родительским категориям
 отчет по доходам
-прикрутить графики
+ввод команды из строки
 сейчас вижу все траты, сделать так чтобы можно было исключать категории
 """
 report_by_all_month()
+report_by_month(year='2017',month=get_month_day('january'))
+con.close()
 
