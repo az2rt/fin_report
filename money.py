77c951cf16c93089d0f3de311a100ed388993439
i.kudryashov <i.kudryashov@corp.mail.ru>
2017-06-19 01:19:45 +0300

    еще чутка поправил кое-что

--- money.py
+++ money.py
@@ -2,6 +2,7 @@
 import argparse
 import json
 import sqlite3
+import Category, Transactions
 from datetime import datetime as dt
 
 year = ['2015', '2016', '2017']
@@ -10,11 +11,11 @@ cur = con.cursor()
 months={
     'january':  ['01','01','31'],
     'february': ['01','02','28'],
-    'march':    ['01','03','30'],
-    'april':    ['01','04','31'],
-    'may':      ['01','05','30'],
-    'june':     ['01','06','31'],
-    'july':     ['01','07','30'],
+    'march':    ['01','03','31'],
+    'april':    ['01','04','30'],
+    'may':      ['01','05','31'],
+    'june':     ['01','06','30'],
+    'july':     ['01','07','31'],
     'august':   ['01','08','31'],
     'september': ['01','09','30'],
     'october':  ['01','10','31'],
@@ -71,9 +72,12 @@ def mysql_fill():
                     cat.return_list(),)
         con.commit()
 
+ # здесь кажется класс не нужен, можно функцией
+
     for transaction in json_row['transactions']:
         trn = Transactions(transaction)
-        transaction['date'] = dt.fromtimestamp(int(transaction['date']) / 1000)
+# а здесь можно проверить на type = 2 и source = 0, чтоб в транзакции попадали только траты
+        trn.date = dt.fromtimestamp(int(transaction['date']) / 1000)
         cur.execute('INSERT INTO transactions (id,name,type,category_id,date,sum,account_id,description,'
                     'source,available) values'
                     '(?,?,?,?,?,?,?,?,?,?)', trn.return_list(),)
@@ -92,29 +96,36 @@ def mysql_select():
 def get_category():
     return cur.execute('select * from category')
 
-
+# тут костыль, потому что between почему справа ограничивает срого ( <)
+# придумать что то с этим
 def last_first_day(year, month):
     first = year + '-' + month[1] + '-' + month[0]
-    last = year + '-' + month[1] + '-' + month[2]
+    last = year + '-' + '0' + str(int(month[1])+1) + '-' + month[0]
     return first, last
 
 
 def report_by_all_month():
-    for current_year in year:
-        for month in months.values():
-            first, last = last_first_day(current_year, month)
-            result = cur.execute("SELECT sum(sum) from transactions t join category c on t.category_id = c.id  "
-                                 "where t.date between ? and ? and t.type = 2 ", (first, last,)).fetchall()
-            if result[0][0] is not None: print(current_year, month, round(result[0][0], 2))
+    current_year = '2017'
+    for month in months.values():
+        first, last = last_first_day(current_year, month)
+        result = cur.execute("SELECT sum(sum) from transactions t join category c on t.category_id = c.id  "
+                                 "where t.date between ? and ? and t.type = 2 and source = 0", (first, last,)).fetchall()
+        if result[0][0] is not None: print(first, round(result[0][0], 2))
 
-
+# print(1,2) выведет на печать (1,2) и терминал если русские буквы выведет их аски код
 def report_by_month(year,month):
     first, last = last_first_day(year, get_month_day(month))
-    result = cur.execute("SELECT sum(sum) from transactions where date between  ? and ?", (first, last,)).fetchall()
-    print(result)
+    result = cur.execute("SELECT c.name, sum(sum) from transactions t join category c on t.category_id = c.id "
+                         "where t.date between  ? and ? and t.source = 0 and t.type = 2 group by t.category_id "
+                         "order by sum(sum) desc",
+                         (first, last,)).fetchall()
+    #import pdb; pdb.set_trace()
+    for i in result:
+
+        print i[0], i[1]
 
 def report_by_year(year):
-    return cur.execute("SELECT sum(sum) from transactions where date between  ? and ?", (year, year,).fetchall())
+    return cur.execute("SELECT sum(sum) from transactions where date between  ? and ? and source=", (year, year,).fetchall())
 
 def report_by_income():
     pass
@@ -137,15 +148,12 @@ if __name__ == '__main__':
     parser.add_argument('--all', action='store_true', help='Report by all month')
     parser.add_argument('-m', '--month', help='Report by one month. Key works only witn --year')
     parser.add_argument('-y', '--year')
-    parser.add_argument('-f', '--fill')
+    parser.add_argument('--fill')
     args = parser.parse_args()
     if args.all:
         report_by_all_month()
     elif args.month and args.year:
         report_by_month(args.year, args.month)
-    elif args.year:
-        mysql_fill()
-
 
 
 
